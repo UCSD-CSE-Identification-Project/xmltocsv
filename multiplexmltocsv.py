12088978411fbc255e7633199364aa0e9a8ac2ee
Agnes Garcia <agnesgarcia@agness-mbp.dynamic.ucsd.edu>
2019-01-22 10:58:42 -0800

    Can parse multiple xml files

--- multiplexmltocsv.py
+++ multiplexmltocsv.py
@@ -3,18 +3,17 @@ import xml.etree.ElementTree as ET
 import csv
 
 filenames = glob.glob('./CSE141FA17/*.xml')
-print filenames
 
-# parse each xml file
-for filename in filenames:
-    print filename
-    tree = ET.parse(filename)
-    root = tree.getroot()
+# write question number, clicker ID, and answers to csv
+with open('cse141fa17clicker.csv', 'a') as r:
+    writer = csv.writer(r)
+    writer.writerow(['Question', 'Clicker ID', 'Final Response'])   # csv header
 
-    # write question number, clicker ID, and answers to csv
-    with open('cse141fa17clicker.csv', 'w') as r:
-        writer = csv.writer(r)
-        writer.writerow(['Question', 'Clicker ID', 'Final Response'])   # csv header
+    # parse each xml file
+    for filename in filenames:
+        print filename
+        tree = ET.parse(filename)
+        root = tree.getroot()
 
         # traverse each question (p block)
         for question in root.iter('p'):
@@ -27,4 +26,3 @@ for filename in filenames:
 
                 # write each row
                 writer.writerow([idx, clickerID, answer])
-                print idx, ",", clickerID, ",", answer
