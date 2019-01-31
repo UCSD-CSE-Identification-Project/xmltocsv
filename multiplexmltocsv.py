083cd1ea2352b94344a60388b333ff583aa7f7c2
Agnes Garcia <agnesgarcia@agness-mbp.dynamic.ucsd.edu>
2019-01-30 21:27:47 -0800

    Changed format of csv files

--- multiplexmltocsv.py
+++ multiplexmltocsv.py
@@ -2,27 +2,54 @@ import glob
 import xml.etree.ElementTree as ET
 import csv
 
-filenames = glob.glob('./CSE141FA17/*.xml')
+filenames = sorted(glob.glob('./CSE141FA17/*.xml'))
+# images = glob.glob('./CSE141FA17Images/*.jpg')
+print filenames
+# print images
+question_list = []
+
+# parse each xml file
+for filename in filenames:
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    # Store question number in array
+    for questionNum in root.iter('p'):
+        index = questionNum.get('idx')
+        question_list.append(index)
+
+print question_list
+print len(question_list)
 
 # write question number, clicker ID, and answers to csv
-with open('cse141fa17clicker.csv', 'a') as r:
-    writer = csv.writer(r)
-    writer.writerow(['Question', 'Clicker ID', 'Final Response'])   # csv header
+with open('cse141fa17clicker.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerow(['Clicker ID'] + question_list)   # csv header
 
-    # parse each xml file
-    for filename in filenames:
-        print filename
-        tree = ET.parse(filename)
-        root = tree.getroot()
+    tree = ET.parse(filenames[0])
+    root = tree.getroot()
 
-        # traverse each question (p block)
-        for question in root.iter('p'):
-            idx = question.get('idx')
+    question = root.find('p')
 
-            # traverse each student (v block)
-            for student in question.iter('v'):
-                clickerID = student.get('id')
-                answer = student.get('ans')
+    # Lists the students' clicker ID's
+    clicker_list = []
+    for student in question.iter('v'):
+        clickerID = student.get('id')
+        clicker_list.append(clickerID)
 
-                # write each row
-                writer.writerow([idx, clickerID, answer])
+    for clicker in clicker_list:
+        answers_list = [clicker]
+
+        for filename in filenames:
+            for student in root.iter('v'):
+                remote = student.get('id')
+
+                if clicker == remote:
+                    ans = student.get('ans')
+                    answers_list.append(ans)
+
+        writer.writerow(answers_list)
+        print answers_list
+
+print clicker_list
+print len(clicker_list)
--- xmltocsv.py
+++ xmltocsv.py
@@ -3,21 +3,40 @@ import csv
 
 tree = ET.parse('lec01.xml')
 root = tree.getroot()
+question_list = []
 
-# write question number, clicker ID, and answers to csv
-with open('clickerresponses.csv', 'w') as r:
-    writer = csv.writer(r)
-    writer.writerow(['Question', 'Clicker ID', 'Final Response'])   # csv header
+# Store question number in array
+for questionNum in root.iter('p'):
+    index = questionNum.get('idx')
+    question_list.append(index)
 
-    # traverse each question (p block)
-    for question in root.iter('p'):
-        idx = question.get('idx')
+print question_list
 
-        # traverse each student (v block)
-        for student in question.iter('v'):
-            clickerID = student.get('id')
-            answer = student.get('ans')
+# write clicker ID, each question, and answers to csv
+with open('clickerresponses.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerow(['Clicker ID'] + question_list)   # csv header
 
-            # write each row
-            writer.writerow([idx, clickerID, answer])
-            print idx, ",", clickerID, ",", answer
+    question = root.find('p')
+
+    # Lists the students' clicker ID's
+    clicker_list = []
+    for student in question.iter('v'):
+        clickerID = student.get('id')
+        clicker_list.append(clickerID)
+
+    for clicker in clicker_list:
+        answers_list = [clicker]
+        for student in root.iter('v'):
+            remote = student.get('id')
+
+            if clicker == remote:
+                ans = student.get('ans')
+                answers_list.append(ans)
+
+        writer.writerow(answers_list)
+        print answers_list
+
+print clicker_list
+print len(clicker_list)
+
